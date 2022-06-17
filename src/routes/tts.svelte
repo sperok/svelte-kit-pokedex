35174fea1833694ab3168e4f47a3f06fc87c9d5b
Spero <spero@auli.tech>
2022-06-17 12:21:13 -0700

    adding tts

---
+++ src/routes/tts.svelte
@@ -0,0 +1,21 @@
+
+<svelte:head>
+  <title>Web Speech API</title>
+</svelte:head>
+
+<style>
+  
+</style>
+
+<h1>Pick your test</h1>
+<ul>
+  <li><a href="phrase-matcher">Phrase matcher</a></li>
+  <li><a href="synthesis">Speak easy synthesis</a></li>
+  <li><a href="color-changer">Speech color changer</a></li>
+</ul>
+<h2>More information</h2>
+<ul>
+  <li><a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API">Web Speech API</a></li>
+  <li><a href="https://github.com/mdn/web-speech-api">The code used on these pages.</a></li>
+</ul>
+
